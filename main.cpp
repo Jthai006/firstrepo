228fadbd1368aa98c19e0bbe4f1285034b049ee9
Jonathan Thai <jthai006@hammer.cs.ucr.edu>
2017-10-04 15:51:34 -0700

    added user input

--- main.cpp
+++ main.cpp
@@ -1,3 +1,15 @@
+include <iostream>
+#include <string>
+
+int main()
+{
+    std::string name;
+    std::cout << "What is your name?" << std::endl;
+    std::cin >> name;
+    std::cout << "Hello " << name << "!" << std::endl;
+
+    return 1;
+}
 
 #include <iostream>
 
